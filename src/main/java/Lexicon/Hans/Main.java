51c5a414408304d994aa09243ba4320599de6e55
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-04-15 11:08:33 +0200

    mod Main.java
    bug fix

--- src/main/java/Lexicon/Hans/Main.java
+++ src/main/java/Lexicon/Hans/Main.java
@@ -5,19 +5,16 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         //Exercise 1: Ask the user to input two integers then divide and handle division by zero.
-        int firstNumber, secondNumber;
+        int firstNumber, secondNumber = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input first number: ");
         firstNumber = scanner.nextInt();
-        while (true) {
-            System.out.print("Input second number: ");
+        System.out.print("Input second number: ");
             try {
                 secondNumber = scanner.nextInt();
-                break;
             } catch (ArithmeticException e) {
-                System.out.println("Error, divide by zero, please try again.");
+                System.out.println(e.getMessage());
             }
-        }
         System.out.println(firstNumber + "/" + secondNumber + " = " + firstNumber/secondNumber);
     }
 }
